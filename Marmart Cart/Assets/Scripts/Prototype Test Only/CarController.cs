be9dde8d45876d93303ab561da62c8cb989868cb
LiangMei-Leon <77693263+LiangMei-Leon@users.noreply.github.com>
2024-10-05 00:51:26 -0700

    add basic control script to test desired camera setting
    + placeholder of car model
    + testing level map
    + (test only) script to drive the car
    + different camera preset (1st person, 3rd person over shoulder, top-down)

---
+++ Marmart Cart/Assets/Scripts/Prototype Test Only/CarController.cs
@@ -0,0 +1,32 @@
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float moveSpeed = 50f;
+    public float maxSpeed = 15f;
+    public float drag = 0.98f;
+    public float steerAngle = 20f;
+    public float traction = 1f;
+    private Vector3 moveForce;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveForce += transform.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += moveForce * Time.deltaTime;
+
+        float steerInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerInput * moveForce.magnitude * steerAngle * Time.deltaTime);
+        moveForce *= drag;
+        moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
+
+        moveForce = Vector3.Lerp(moveForce.normalized, transform.forward, traction * Time.deltaTime) * moveForce.magnitude;
+    }
+}
